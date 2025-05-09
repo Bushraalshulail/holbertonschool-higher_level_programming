b832f663bf426b1cf0e45058e2615406b6558682
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-05-09 00:39:55 +0000

    add file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
 def uppercase(str):
+    result = ""
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):  # check if the char is lower
-            print(chr(ord(char) - 32), end='')  # convert to upper ASCII
+        if ord('a') <= ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
         else:
-            print(char, end='')  # print the char as is if it's not a lower
-    print()  # print a new line at the end
+            result += char
+    print("{}".format(result))
+
+
+# Temporary test
+uppercase("Best School 98 Battery street")
