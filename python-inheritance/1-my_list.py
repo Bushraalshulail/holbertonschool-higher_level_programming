2b53731c8390b4945f12e0436aeb9f001afd161b
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-05-26 15:29:11 +0000

    update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This module defines the MyList class, which inherits from the built-in list.
-It adds a method to print the list in ascend sort order without modifying it.
+It adds a method to print the list in ascending sort order without modify it.
 """
 
 
@@ -9,12 +9,12 @@ class MyList(list):
     """
     MyList is a subclass of the built-in list.
     It provides a method to print the list in ascending order.
-    Assumes all list elements are integers.
+    All elements in the list are expected to be integers.
     """
 
     def print_sorted(self):
         """
-        Prints the elements of the list in ascending sorted order.
-        The original list remains unchanged.
+        Prints the list elements in ascending sorted order.
+        This method does not modify the original list.
         """
         print(sorted(self))
