2eb424a3eb6cbe6ffee6322c684479b265b78550
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-06-03 14:50:11 +0000

    add

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines class_to_json function."""
+
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object"""
+    """Returns the dictionary description for JSON serialization of an object."""
     return obj.__dict__
