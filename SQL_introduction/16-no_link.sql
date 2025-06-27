90bfb6f36eaf52365fd598d390d7531e091e5817
Bushraalshulail <Bushraalshulail@hotmail.com>
2025-06-27 12:19:28 +0000

    add 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,23 +1,5 @@
-16. Say my name
-mandatory
-Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-
-Don’t list rows where the name column does not contain a value
-Results should display the score and the name (in this order)
-Records should be listed by descending score
-The database name will be passed as an argument to the mysql command
-In this example, new data have been added to the table second_table.
-
-guillaume@ubuntu:~/$ cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-Enter password: 
-score   name
-18  Aria
-12  Aria
-10  John
-10  Bob
-guillaume@ubuntu:~/$ 
-Repo:
-
-GitHub repository: holbertonschool-higher_level_programming
-Directory: SQL_introduction
-File: 16-no_link.sql
+-- List records from second_table with non-empty name ordered by descending score
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL AND name != ''
+ORDER BY score DESC;
